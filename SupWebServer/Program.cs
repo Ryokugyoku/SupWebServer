74a800418d982a6c1542e2ed70b84ec40ace5ffe
ryokugyoku <ryoku@blueremarks.com>
2025-07-05 12:44:38 +0900

    SwaggerUIの設定

--- SupWebServer/Program.cs
+++ SupWebServer/Program.cs
@@ -1,19 +1,18 @@
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+builder.Services.AddControllers();   
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
 builder.Services.AddEndpointsApiExplorer(); // ← UI には必須
 builder.Services.AddSwaggerGen();           // ← ドキュメント生成
+
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-app.MapOpenApi();
+app.UseSwagger();  
 app.UseSwaggerUI(); 
-app.MapControllers(); 
 //一旦コメントアウト
-//app.UseHttpsRedirection();
+app.UseHttpsRedirection();
 
 app.Run();
 
